977a50e5f0beef1cf5cb6a63280e31b3d8e4e811
Delwo Athauda <delwo2003@gmail.com>
2025-08-07 22:14:06 +0530

    Update routing and navigation; replace Coming Soon page with HomePage, add GetStartedNowPage and LimitedSpecialPage, and adjust navigation links and styles.

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,19 @@ import HomePage from "@/pages/home.page.jsx";
 import AboutPage from "@/pages/about.page.jsx";
 import PortfolioPage from "@/pages/portfolio.page.jsx";
 import ContactPage from "@/pages/contact.page.jsx";
-import ComingSoon from "@/pages/coming-soon.page.jsx";
+import GetStartedNowPage from "@/pages/get-started-now.page.jsx";
+import LimitedSpecialPage from "@/pages/limited-special.page.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ComingSoon />} />
-        {/* <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/contact" element={<ContactPage />} /> */}
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/get-started-now" element={<GetStartedNowPage />} />
+        <Route path="/limited-special" element={<LimitedSpecialPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
