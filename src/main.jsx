b9daac0b9d1531ebaeda337156bd7f578ee83ebd
Delwo Athauda <officeitdev01@gmail.com>
2025-08-07 21:52:33 +0530

    Add new components and pages; implement navigation and button UI; update styles and configuration files.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/home.page.jsx";
+import AboutPage from "@/pages/about.page.jsx";
+import PortfolioPage from "@/pages/portfolio.page.jsx";
+import ContactPage from "@/pages/contact.page.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
