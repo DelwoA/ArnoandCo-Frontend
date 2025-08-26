4c9180641ba9ec665bfac506adf7454261ad307f
Delwo Athauda <officeitdev01@gmail.com>
2025-08-27 01:36:48 +0530

    Update Hero component text for clarity and impact; comment out PricingCards in HomePage for future use.

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
       <TryYourNumbersCalculator />
       <RealResultsCaseStudy />
       <WorkTimeline />
-      <PricingCards />
+      {/* <PricingCards /> */}
       {/* <TestimonialsCarousel /> */}
       <FinalCTA />
     </>
